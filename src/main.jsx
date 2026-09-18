fc72a4beaef51084a6d41880e98d251c82d28bc7
VoidcallerOC <voidcalleroc@gmail.com>
2026-09-18 01:59:21 -0400

    3D photo slabs: cropped photos, idle yaw, pointer tilt matching packs

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './packs.css'
 import './motion.css'
+import './slabs.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
